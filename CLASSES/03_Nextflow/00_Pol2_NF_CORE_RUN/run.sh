ba54a87ca3eed2374fc61975bd56a3d4caf024ef
Dakota Hunt <dahu3610@colorado.edu>
2023-04-19 13:59:53 -0600

    April 19 push

--- CLASSES/03_Nextflow/00_Pol2_NF_CORE_RUN/run.sh
+++ CLASSES/03_Nextflow/00_Pol2_NF_CORE_RUN/run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH -p short
-#SBATCH --job-name=YFTF_Chipseq
+#SBATCH --job-name=dahu3610_Pol2_test
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=john.rinn@colorado.edu
+#SBATCH --mail-user=dahu3610@colorado.edu
+#SBATCH --output=nextflow.out
+#SBATCH --error=nextflow.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mem=6gb
-#SBATCH --time=11:00:00
-#SBATCH --output=nextflow.out
-#SBATCH --error=nextflow.erry
+#SBATCH --time=10:00:00
 pwd; hostname; date
-echo "Lets go"
+echo "Lets do chipseq"
 module load singularity/3.1.1
 nextflow run nf-core/chipseq -r 1.2.1 \
 -profile singularity \
@@ -20,7 +20,7 @@ nextflow run nf-core/chipseq -r 1.2.1 \
 --gtf /scratch/Shares/rinnclass/CLASS_2023/data/data/genomes/gencode.v32.annotation.gtf \
 --macs_gsize 3.2e9 \
 --blacklist /scratch/Shares/rinnclass/CLASS_2023/data/data/genomes/hg38-blacklist.v2.bed \
---email john.rinn@colorado.edu \
+--email dahu3610@colorado.edu \
 -resume \
 -c nextflow.config
 date
